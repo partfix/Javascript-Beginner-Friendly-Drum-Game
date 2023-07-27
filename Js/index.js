5ea968ff97fe378329b2622f75327c01b2cd64c6
John Clarence Palma <johnclarencepalma7@gmail.com>
2023-07-27 19:35:19 +0800

    update

--- Js/index.js
+++ Js/index.js
@@ -1,6 +1,9 @@
 //let i = 0 (if i is less than the document) i++ until it return false
 for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
-	document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-		alert("hello");
+	document.querySelectorAll(".drum")[0].addEventListener("click", function () {
+		//initialise Object
+		let audio = new Audio("sounds/crash.mp3");
+		audio.play();
+		this.style.color = "midnightBlue";
 	});
 }
