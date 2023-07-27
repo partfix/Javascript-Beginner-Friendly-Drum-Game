d5de29dc420bbd39e91194d8294f3878c8337c79
John Clarence Palma <johnclarencepalma7@gmail.com>
2023-07-27 13:45:45 +0800

    add files

--- Js/index.js
+++ Js/index.js
@@ -1,4 +1,4 @@
-//let i = 0 (if i is less than the object) i++ until it will go false
+//let i = 0 (if i is less than the document) i++ until it return false
 for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
 	document.querySelectorAll(".drum")[i].addEventListener("click", function () {
 		alert("hello");
