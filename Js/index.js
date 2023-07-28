91764eb67adb841a059a06ac68f2dffc0aacbf09
John Clarence Palma <johnclarencepalma7@gmail.com>
2023-07-28 15:05:18 +0800

    update content

--- Js/index.js
+++ Js/index.js
@@ -1,6 +1,6 @@
 //let i = 0 (if i is less than the document) i++ until it return false
 for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
-	document.querySelectorAll(".drum")[0].addEventListener("click", function () {
+	document.querySelectorAll(".drum")[i].addEventListener("click", function () {
 		//initialise Object
 		let audio = new Audio("sounds/crash.mp3");
 		audio.play();
