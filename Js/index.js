a8893e0b531c62451bae99f3fe40434ac6e79e80
John Clarence Palma <johnclarencepalma7@gmail.com>
2023-07-29 18:19:28 +0800

    add comment

--- Js/index.js
+++ Js/index.js
@@ -1,5 +1,7 @@
 //let i = 0 (if i is less than the document) i++ until it return false
 
+//you can use a ()=>  for a function
+
 //detecting button press
 for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
 	document.querySelectorAll(".drum")[i].addEventListener("click", function () {
